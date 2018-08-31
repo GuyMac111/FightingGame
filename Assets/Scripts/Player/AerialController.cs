ef9335c746909a0b5f7ecffd18586b3081f50d3d
gmclean <gmclean@gsngames.com>
2018-08-31 22:33:32 +0100

    [Week 2] Moved most of the Aerial physics logic into an "AerialSession" (formerly called "Jump"). The jump controller now only exists to interact with Unity. This approach can be strategised and be abstracted out to cater for different jump varieties, e.g. Mewtwo jump.

---
+++ Assets/Scripts/Player/AerialController.cs
@@ -0,0 +1,91 @@
+﻿using Player.Physics;
+using UnityEngine;
+
+namespace Player {
+    [RequireComponent(typeof(Rigidbody))]
+    public class AerialController: MonoBehaviour {
+
+        private Rigidbody _rigidbody;
+        public PlayerState currentState = PlayerState.AIRBOURNE_FALLING;
+
+        public float risingForce = 0.3f;
+        public float fallingForce = 0.7f;
+        public bool enforceMaxJumpHeight = false;
+        public float maxJumpHeight = 1;
+        public int maxNumJumps = 1;
+        
+
+        private AerialSession currentAerialSession;
+
+        void Start() {
+            if (_rigidbody == null) {
+                _rigidbody = gameObject.GetComponent<Rigidbody>();
+            }
+            _rigidbody.isKinematic = false;
+        }
+
+        void Update() {
+            if (Input.GetKeyDown("space")) {
+                OnJumpInput();
+            }
+            
+            switch (currentState) {
+                case PlayerState.AIRBOURNE_RISING:
+                case PlayerState.AIRBOURNE_FALLING:
+                    HandleAirbourneState();
+                    break;
+            }
+        }
+
+        private void OnJumpInput() {
+            if (maxNumJumps <= 0) {
+                //special case for characters we may not wish to have a jump,
+                //or a potential mechanic to disable the opponent's jump ability.
+                return;
+            }
+            if (currentAerialSession == null) {
+                currentAerialSession = new AerialSession(maxJumpHeight, enforceMaxJumpHeight, maxNumJumps, this, _rigidbody);
+            }
+            currentAerialSession.RequestJump();
+        }
+        
+        private void HandleAirbourneState() {
+            if (currentAerialSession == null) {
+                //Something to handle the sudden disappearance of a floor. E.g. game start, or walking off an edge
+                currentAerialSession = new AerialSession(maxJumpHeight, enforceMaxJumpHeight, maxNumJumps-1,this, _rigidbody);
+            }
+            currentAerialSession.HandleAerialState();
+        }
+        
+        private void OnCollisionEnter(Collision other) {
+            EnvironmentID collidedWithWhat = other.gameObject.GetComponent<EnvironmentID>();
+            if (!collidedWithWhat) {
+                return;
+            } 
+            switch(collidedWithWhat.envType) {
+                case EnvironmentID.EnvironmentObject.FLOOR:
+                    HandleCollisionWithFloor();
+                    break;
+            }
+        }
+
+        private void HandleCollisionWithFloor() {
+            switch (currentState) {
+                case PlayerState.AIRBOURNE_FALLING:
+                    OnGroundCollision();
+                    break;
+            }
+        }
+
+        private void OnGroundCollision() {
+            currentAerialSession.Land();
+            currentAerialSession = null;
+        }
+    }
+    
+    public enum PlayerState {
+        GROUNDED,AIRBOURNE_RISING,AIRBOURNE_FALLING
+    }
+    
+    
+}
---
+++ Assets/Scripts/Player/Physics/AerialSession.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using UnityEditor;
+using UnityEngine;
+namespace Player.Physics {
+    public class AerialSession {
+        private float maxJumpHeight;
+        private int maxNumJumps;
+        private int jumpsUsed;
+        private float startingJumpHeight;
+        private bool shoulEnforceMaxJumpHeight;
+        private AerialController aerialController;
+        private Rigidbody rigidBody;
+        
+        public AerialSession(float maximumJumpHeight, bool enforceMaxJumpHeight, int maximumNumJumps, AerialController controller, Rigidbody rigidBody) {
+            maxJumpHeight = maximumJumpHeight;
+            shoulEnforceMaxJumpHeight = enforceMaxJumpHeight;
+            maxNumJumps = maximumNumJumps;
+            aerialController = controller;
+            jumpsUsed = 0;
+            startingJumpHeight = aerialController.transform.position.y;
+            this.rigidBody = rigidBody;
+        }
+
+        public void RequestJump() {
+            if (canJump) {
+                rigidBody.isKinematic = false;
+                /////////
+                //Set velocity to be a fraction above what it was, to avoid immediately going into the fall state
+                rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0.01f, rigidBody.velocity.z);
+                /////////
+                IncJumpCounter();
+                ResetJumpStartingPos();
+                aerialController.currentState = PlayerState.AIRBOURNE_RISING;
+                rigidBody.AddForce(0, aerialController.risingForce, 0, ForceMode.Impulse);
+            }
+        }
+
+        public void InitiateFall() {
+            Debug.Log("AerialSession:: InitiateFall()");
+            aerialController.currentState = PlayerState.AIRBOURNE_FALLING;
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z);
+            rigidBody.AddForce(0, -aerialController.fallingForce, 0, ForceMode.Impulse);
+        }
+
+        public void Land() {
+            Debug.Log("AerialSession:: Land()");
+            aerialController.currentState = PlayerState.GROUNDED;
+            rigidBody.isKinematic = true;
+        }
+
+        public void HandleAerialState() {
+            switch (aerialController.currentState) {
+                case PlayerState.AIRBOURNE_RISING:
+                    if (!hasUpwardVerticalMomentum || hasReachedMaxJumpHeight) {
+                        InitiateFall();
+                    }
+                    break;       
+            }
+        }
+        
+        private bool canJump {
+            get {
+                bool result = jumpsUsed < maxNumJumps;
+                Debug.Log("canJump = " + result);
+                return jumpsUsed < maxNumJumps;
+            }
+        }
+        
+        private bool hasUpwardVerticalMomentum {
+            get { return rigidBody.velocity.y > 0; }
+        }
+        
+        private bool hasReachedMaxJumpHeight {
+            get {
+                bool result = aerialController.transform.position.y - startingJumpHeight >=  maxJumpHeight 
+                              && shoulEnforceMaxJumpHeight;
+                if(result) {
+                    Debug.Log("AerialSession:: Maximum jump height reached and is being enforced");
+                }
+                return result;
+            } 
+        }
+        
+        private void ResetJumpStartingPos() {
+            startingJumpHeight = aerialController.transform.position.y;
+        }
+        
+        private void IncJumpCounter() {
+            jumpsUsed++;
+        }
+    }
+}
